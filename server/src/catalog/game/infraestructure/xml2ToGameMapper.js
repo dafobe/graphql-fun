21fba7d514b5a8dda7e9d2526f0b897f1262aedb
David Fonollosa <dafobe@gmail.com>
2021-04-14 17:58:05 +0200

    update mapper in order to manage attributes also

--- server/src/catalog/game/infraestructure/xml2ToGameMapper.js
+++ server/src/catalog/game/infraestructure/xml2ToGameMapper.js
@@ -2,10 +2,13 @@ import gameFactory from '../domain/game.js';
 import xmlParser from 'fast-xml-parser';
 
 export const xml2ToGameMapper = (xml) => {
-    const parsedXml = xmlParser.parse(xml);
+    const parsedXml = xmlParser.parse(xml, {
+        ignoreAttributes: false
+    });
 
     return gameFactory({
-        name: parsedXml.items.item.name[0],
+        bggId: parsedXml.items.item['@_id'],
+        name: parsedXml.items.item.name[0]['@_value'],
         description: parsedXml.items.item.description,
         thumbnail: parsedXml.items.item.thumbnail
     })
